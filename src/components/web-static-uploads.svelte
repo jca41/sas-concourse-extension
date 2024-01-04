ea7b6145791c9af545b7d2af2f9e0291888bcdd0
João Café <joao.cafe@sky.uk>
2024-01-04 12:22:24 +0000

    fix: hide uploads if not present

--- src/components/web-static-uploads.svelte
+++ src/components/web-static-uploads.svelte
@@ -7,21 +7,21 @@
   $: uploads = parseUploads(data);
 </script>
 
-<div>
-  <h2 class="mb-2 text-xl font-semibold">Uploads</h2>
-  {#each uploads as [section, files]}
-    {#if section !== "default"}
-      <div class="divider">
-        <h3 class="mb-2 text-lg font-semibold">{section}</h3>
-      </div>
-    {/if}
+{#if uploads.length}
+  <div>
+    <h2 class="mb-2 text-xl font-semibold">Uploads</h2>
+    {#each uploads as [section, files]}
+      {#if section !== "default"}
+        <div class="divider">
+          <h3 class="mb-2 text-lg font-semibold">{section}</h3>
+        </div>
+      {/if}
 
-    <div class="grid grid-cols-2 gap-2">
-      {#each files as entry}
-        <UploadFile {entry} />
-      {/each}
-    </div>
-  {:else}
-    <p>No uploads found.</p>
-  {/each}
-</div>
+      <div class="grid grid-cols-2 gap-2">
+        {#each files as entry}
+          <UploadFile {entry} />
+        {/each}
+      </div>
+    {/each}
+  </div>
+{/if}
